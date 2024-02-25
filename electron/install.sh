90525e26a985129dc8e19f20f7e6b8a5fc22583d
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2024-02-25 22:17:28 +0100

    add electron example

---
+++ electron/install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -xEeuo pipefail
+
+BUILDROOT=$1
+ELECTRON_URL="https://github.com/electron/electron/releases/download/v29.0.1/electron-v29.0.1-linux-x64.zip"
+LIBDIR="/usr/lib/omnipackage_example_electron"
+APPDIR=$LIBDIR/resources/app/
+
+install -d -m755 $BUILDROOT$APPDIR
+wget --no-verbose -O electron.zip $ELECTRON_URL
+unzip electron.zip -d $BUILDROOT$LIBDIR/
+rm electron.zip
+install -d -m755 $BUILDROOT/usr/bin/
+install -m644 $(ls -I ".omnipackage" -I ".gitignore" -I ".node-version" -I "install.sh" -I "node_modules" -I "debian") $BUILDROOT$APPDIR
+ln -sf $LIBDIR/electron $BUILDROOT/usr/bin/omnipackage_example_electron
