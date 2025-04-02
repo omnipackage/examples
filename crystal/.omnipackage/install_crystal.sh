0d488b086f3b97c5e84f3036ac7624bdc1b621f4
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2025-04-02 22:44:59 +0200

    update few more examples to use package_name

--- crystal/.omnipackage/install_crystal.sh
+++ crystal/.omnipackage/install_crystal.sh
@@ -3,8 +3,8 @@
 set -xEeuo pipefail
 
 if crystal --version; then
-    exit 0
+  exit 0
 fi
 
-curl -L https://github.com/crystal-lang/crystal/releases/download/1.11.2/crystal-1.11.2-1-linux-x86_64.tar.gz | tar -zx -C /usr/local
-ln -s /usr/local/crystal-1.11.2-1/bin/* /usr/bin/
+curl -L https://github.com/crystal-lang/crystal/releases/download/1.15.1/crystal-1.15.1-1-linux-x86_64.tar.gz | tar -zx -C /usr/local
+ln -s /usr/local/crystal-1.15.1-1/bin/* /usr/bin/
