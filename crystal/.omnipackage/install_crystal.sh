1d0aff7efc2f75aae53a929f7cd4798500ff470f
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2025-07-12 08:13:42 +0200

    fix tauri and crystal

--- crystal/.omnipackage/install_crystal.sh
+++ crystal/.omnipackage/install_crystal.sh
@@ -6,5 +6,7 @@ if crystal --version; then
   exit 0
 fi
 
-curl -L https://github.com/crystal-lang/crystal/releases/download/1.15.1/crystal-1.15.1-1-linux-x86_64.tar.gz | tar -zx -C /usr/local
-ln -s /usr/local/crystal-1.15.1-1/bin/* /usr/bin/
+VERSION=1.16.3
+
+curl -L https://github.com/crystal-lang/crystal/releases/download/$VERSION/crystal-$VERSION-1-linux-x86_64.tar.gz | tar -zx -C /usr/local
+ln -s /usr/local/crystal-$VERSION-1/bin/* /usr/bin/
